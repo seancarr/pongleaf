a08f5578c74d1bd1ecd534e4a3faca6a5a215823
Sean Carr <git@seancarr.net>
2011-10-27 18:05:19 -0700

    remove location

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -19,19 +19,4 @@ describe ApplicationHelper do
     end
   end
 
-  describe "#location" do
-    subject { helper.location }
-
-    context "when no location is configured" do
-      it { should == "NYC" }
-    end
-
-    context "when a location is configured" do
-      before do
-        ENV["PIVOT_PONG_LOCATION"] = "Starbase Alpha"
-      end
-
-      it { should == "Starbase Alpha" }
-    end
-  end
 end
